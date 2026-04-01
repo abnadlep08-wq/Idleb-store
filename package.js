f26c7d6182402f3512ada05c4dcdb43443739a6a
abnadlep08-wq <abnadlep08@gmail.com>
2026-04-01 05:39:04 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "idleb-x-store",
+  "version": "5.0.0",
+  "type": "module",
+  "scripts": {
+    "start": "node store-ultimate.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "^6.6.0",
+    "qrcode-terminal": "^0.12.0",
+    "pino": "^8.17.2",
+    "@hapi/boom": "^10.0.1"
+  }
+    }
